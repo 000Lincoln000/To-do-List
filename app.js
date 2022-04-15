01403cdfca057f2e4b74185ea8276e52063d224d
Lincoln <jeanlincol@gmail.com>
2022-04-15 12:07:50 -0300

    refatoração do codigo

--- app.js
+++ app.js
@@ -2,41 +2,86 @@ const formAddTodo = document.querySelector(".form-add-todo")
 const todosContainer = document.querySelector(".todos-container")
 const searchInput = document.querySelector(".form-search input")
 
-formAddTodo.addEventListener("submit", event => {
-  event.preventDefault()
+const getTextOfANewtodo = () => {
   const inputValue = event.target.add.value.trim()
-  const theresAnyText = inputValue.length
-  if (theresAnyText) {
-    todosContainer.innerHTML += `
-  <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+  return inputValue
+}
+
+const addingTodoElement = () => {
+  const inputValue = getTextOfANewtodo()
+  const newTodo = (todosContainer.innerHTML += `
+  <li data-li="todo" class="list-group-item d-flex justify-content-between align-items-center text-light">
       <span>${inputValue}</span>
       <i class="far fa-trash-alt delete"></i>
-  </li>`
+  </li>`)
+
+  return newTodo
+}
+
+const addingEmptyTodoElement = () => {
+  const emptyTodo = (todosContainer.innerHTML += `
+  <li data-li="todo" class="list-group-item d-flex justify-content-between align-items-center text-light">
+      <span>(Todo vazia)</span>
+      <i class="far fa-trash-alt delete"></i>
+  </li>`)
+
+  return emptyTodo
+}
+
+const verifyTheNewTodoTextContent = () => {
+  const inputValue = getTextOfANewtodo()
+  const theresAnyText = inputValue.length
+
+  if (theresAnyText) {
+    const newTodo = addingTodoElement()
   } else {
-    alert("você não digitou nenhum valor no item a fazer")
+    const emptyTodo = addingEmptyTodoElement()
   }
+}
+
+const getANewTodo = event => {
+  event.preventDefault()
+  const insertTodoIntoList = verifyTheNewTodoTextContent()
   event.target.reset()
-})
+}
 
-todosContainer.addEventListener("click", event => {
+const selectNRemoveATodo = event => {
   const clickedElement = event.target
+  const li = clickedElement.closest('[data-li="todo"]')
   if (Array.from(clickedElement.classList).includes("delete")) {
-    clickedElement.parentElement.remove()
+    li.remove()
   }
-})
+}
 
-searchInput.addEventListener("input", event => {
+const getTextOfSearchInput = () => {
   const inputValue = event.target.value.trim().toLowerCase()
-  Array.from(todosContainer.children)
+  return inputValue
+}
+
+const showOrHideATodo = () => {
+  const inputValue = getTextOfSearchInput()
+  const hide = Array.from(todosContainer.children)
     .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
     .forEach(todo => {
       todo.classList.remove("d-flex")
       todo.classList.add("hidden")
     })
-  Array.from(todosContainer.children)
+  const show = Array.from(todosContainer.children)
     .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
     .forEach(todo => {
       todo.classList.add("d-flex")
       todo.classList.remove("hidden")
     })
-})
+
+  return hide, show
+}
+
+const searchATodo = event => {
+  const hideOrShowATodo = showOrHideATodo()
+}
+
+formAddTodo.addEventListener("submit", getANewTodo)
+
+todosContainer.addEventListener("click", selectNRemoveATodo)
+
+searchInput.addEventListener("input", searchATodo)
